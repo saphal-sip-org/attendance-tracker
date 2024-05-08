d6f07154d34d1a6355ef02e632ee22497eef19a2
Sushil Chaudhary <SushilChaudhary0408>
2024-05-08 15:07:20 +0545

    add animation

--- client/src/routes/pages/Register.jsx
+++ client/src/routes/pages/Register.jsx
@@ -19,8 +19,8 @@ function Register() {
   
 
   return (
-    <div className="w-screen h-screen flex  items-center justify-center bg-gray-100">
-      <img width={400} className='me-10' src={RegisterDrawing} alt="" srcset="" />
+    <div className="w-screen h-screen flex  items-center justify-center bg-gray-100 gap-32">
+      <img width={400} className='me-10 animate-float' src={RegisterDrawing} alt="" srcset="" />
       <form className="flex flex-col justify-between h-[50%] w-96 p-5 relative bg-white rounded-xl shadow-lg">
         <div className="absolute left-0 top-0 w-full h-full z-10 backdrop-blur-sm rounded-xl transition-all flex justify-center items-center">
           <div className="mt-4 border-t-2 pt-4">
