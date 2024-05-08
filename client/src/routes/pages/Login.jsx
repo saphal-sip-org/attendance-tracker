a5bf40bbd85f116421bf3ac7a44ef2f14605726b
sujansijakhow <sujansijakhow@gmail.com>
2024-05-08 14:16:40 +0545

    components added

--- client/src/routes/pages/Login.jsx
+++ client/src/routes/pages/Login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 function Login() {
   return (
     <div>
-        
+      <p>Login</p>
+      
+        <Input label={"Username"}/>
+        <Button label={"Submit"}/>
     </div>
   )
 }
