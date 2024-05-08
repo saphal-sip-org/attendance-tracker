5d3c0a620c339b20e5146fd8942c51039c73c01a
Sushil Chaudhary <SushilChaudhary0408>
2024-05-08 14:27:59 +0545

    Register

--- client/src/routes/pages/Register.jsx
+++ client/src/routes/pages/Register.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useMemo, useState } from "react";
+
+
+import RegisterDrawing from "../../images/login.svg"
+
+
 
 function Register() {
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+
+
+  
+  
+  
+
   return (
-    <div>
-      Register
+    <div className="w-screen h-screen flex  items-center justify-center bg-gray-100">
+      <img width={400} className='me-10' src={RegisterDrawing} alt="" srcset="" />
+      <form className="flex flex-col justify-between h-[50%] w-96 p-5 relative bg-white rounded-xl shadow-lg">
+        <div className="absolute left-0 top-0 w-full h-full z-10 backdrop-blur-sm rounded-xl transition-all flex justify-center items-center">
+          <div className="mt-4 border-t-2 pt-4">
+            
+          </div>
+        </div>
+
+      </form>
+
     </div>
   )
 }
