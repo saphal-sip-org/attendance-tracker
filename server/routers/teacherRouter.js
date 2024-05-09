06f5b0afae1539524bce00e6e85c23ecb7e860ec
milangautamDemon <milangautam071440304@gmail.com>
2024-05-09 20:12:34 +0545

    ManageFolder: controllers

--- server/routers/teacherRouter.js
+++ server/routers/teacherRouter.js
@@ -1,11 +1,12 @@
 import express from "express";
-import teacherRegisterRouter  from "../controllers/teacherRegister.js"
-import teacherLoginRouter from "../controllers/teacherLogin.js";
+import teacherRegisterRouter  from "../controllers/teacherControllers/teacherRegister.js"
+import teacherLoginRouter from "../controllers/teacherControllers/teacherLogin.js";
 
 const router = express.Router();
 
 router.use("/", teacherRegisterRouter);
 router.use("/", teacherLoginRouter);
+router.use("/", )
 
 //exporting teacher router
 export default router;
