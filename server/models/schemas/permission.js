913314019bb28b276561d3c029318c95d2c8e8fc
milangautamDemon <milangautam071440304@gmail.com>
2024-05-11 17:44:19 +0545

    Refactor: permission schema

--- server/models/schemas/permission.js
+++ server/models/schemas/permission.js
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 const permissionSchema = new mongoose.Schema({
     user_id : {
         type : mongoose.Schema.Types.ObjectId,
         required : true
     },
-    permission : {
+    permission : [{
         permission_name : String,
-        permission_value : Number //can use both number or string ( we use 0-create, 1-read, 2-edit, 3-delete)
-    }
+        permission_value : [ Number ] //can use both number or string ( we use 0-create, 1-read, 2-edit, 3-delete)
+    }]
 })
 
-export default Permission = mongoose.model("Permission", permissionSchema);
-export {Permission};
+export const Permission = mongoose.model("Permission", permissionSchema);
+export default Permission;
--- server/models/schemas/schemas.js
+++ server/models/schemas/schemas.js
@@ -2,5 +2,6 @@ import Student from "./student.js";
 import Attendance from "./attendance.js";
 import Teacher from "./teacher.js";
 import Course from "./course.js";
+import Permission from "./permission.js";
 
-export {Student, Attendance, Teacher, Course};
+export {Student, Attendance, Teacher, Course, Permission};
